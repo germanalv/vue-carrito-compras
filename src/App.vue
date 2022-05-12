97508cb0b73eec015161ed0d8cd84a3dfc3d1cec
German Alvarez <germanalv@gmail.com>
2022-05-12 11:56:36 -0300

    Fin proyecto

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,42 @@
 <template>
-  <h1>Carrito</h1>
+  <div class="container">
+    <h1>Carrito</h1>
+    <hr>
+    <Carrito />
+    <div class="row">
+      <!-- el "of" en v-for es para arrays el in es para objeto -->
+      <Card 
+        v-for="producto of productos" :key="producto.id"
+        :producto="producto"
+      />
+       
+    </div>
+
+  </div>
 </template>
 
 <script>
-
-
+import { useStore } from 'vuex'
+import { computed, onMounted } from "vue";
+import Card from "./components/Card";
+import Carrito from './components/Carrito.vue';
 export default {
   name: 'App',
   components: {
+    Card,
+    Carrito
+},
+  setup(){
+    const store = useStore();
+    onMounted( () => {
+      store.dispatch('fetchData')
+    })
     
+    const productos = computed(() => store.state.productos)
+    // const carrito = computed(() => store.state.carrito)
+
+
+    return {productos}
   }
 }
 </script>
